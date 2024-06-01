9057bcbb03439b7c3f126f94b265b94e3bc1fc6a
sara-spreitzhofer <sara.spreitzhofer@tailored-apps.com>
2024-06-01 09:19:02 +0200

    delete model imports

--- train_crossval.py
+++ train_crossval.py
@@ -11,11 +11,6 @@ import sys
 from functools import partial
 
 from models.model_classifier import ResNet, ResidualBlock
-from models.cnn import CNN
-from models.seperable_transformer import SeparableTr
-from models.res_net_50 import ResNet50
-from models.res_net_18 import ResNet18
-from models.res_net_dropout import ResNetDropout
 from models.utils import EarlyStopping, Tee
 from dataset.dataset_ESC50 import ESC50
 import config
